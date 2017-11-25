b6cdd4af1de549a3cf79094d6b30577b97a31793
Guilherme de Lyra <guilyra12@gmail.com>
2017-11-25 15:00:31 -0200

    100% pronto

--- avl.c
+++ avl.c
@@ -3,24 +3,45 @@
 #include <string.h>
 #include <ctype.h>
 #include <stdbool.h>
+#include <ncurses.h>
+#include <sys/resource.h>
+#include <sys/time.h>
 
 #define malc(x) (x *) malloc(sizeof(x))
 #define nl ; printf("\n")
 #define max(a, b) ((a > b) ? (a) : (b))
 
+#define NOME_DICIONARIO "dicioPadrao"
+#define SUCESSO                 0
+#define NUM_ARGS_INCORRETO      1
+#define ARQDICIO_NAOCARREGADO   2
+#define ARQTEXTO_ERROABERTURA   3
+#define ARQTEXTO_ERROLEITURA    4
+#define ERRO_DICIO_NAOCARREGADO 5
+#define TAM_MAX                 45
+
 /* #### ESTRUTURAS DE DADOS ARVORE #### */
 
+
 typedef struct _tree {
-	char conteudo[46];
+	char conteudo[TAM_MAX+1];
 	int h;
 	struct _tree *esq, *dir;
 } arv; arv * raiz;
 
 
+typedef struct _tripair {
+	int qtdErros, qtdPalavras;
+	double tempoCheck;
+} tripair;
+
 /* #### PSEUDO-FUNÇÕES PARA MANIPULAÇÃO DE ARVORE BINÁRIA DE BUSCA #### */
 
-void carregaDicio (char * filename);
-int contaErros (char * filename);
+
+bool carregaDicio (char * filename);
+tripair contaErros (char * filename);
+int contaDicio (arv * root);
+double calcula_tempo(const struct rusage *b, const struct rusage *a);
 arv * busca (arv * raiz, char * el);
 void liberaraiz (arv * ra);
 int height (arv * temp);
@@ -31,25 +52,87 @@ arv * lr (arv * parent);
 arv * rl (arv * parent);
 arv * balance (arv * temp);
 arv * insert (arv * root, char * conteudo);
-void set_height (arv * root, int height); 
 
 
 /* #### MAIN ##### */
 
-int main(){
-	carregaDicio("test.txt");
-	printf("%d\n", contaErros("test1.txt"));
+
+int main (int argc, char * argv[]) {
+	struct rusage tempo_inicial, tempo_final;
+	char * dicionario, * arqTexto;
+	int qtdePalavrasDic;
+	tripair texto;
+	double tempo_carga = 0.0, tempo_calc_tamanho_dic = 0.0, tempo_limpeza_memoria = 0.0;
+	bool carga;
+
+
+	dicionario = (argc == 3) ? argv[1] : NOME_DICIONARIO;
+
+	if (argc != 2 && argc != 3) {
+	    printf("Uso: %s [nomeArquivoDicionario] nomeArquivoTexto\n", argv[0]);
+	    return NUM_ARGS_INCORRETO;
+	}
+
+	getrusage(RUSAGE_SELF, &tempo_inicial);
+		carga = carregaDicio(dicionario);
+	getrusage(RUSAGE_SELF, &tempo_final);
+
+	if (!carga) {
+		printf("Dicionario nao carregado!\n");
+		return ARQDICIO_NAOCARREGADO;
+	}
+
+	tempo_carga = calcula_tempo (&tempo_inicial, &tempo_final);
+
+
+	arqTexto = (argc == 3) ? argv[2] : argv[1];	
+	texto = contaErros(arqTexto);
+
+	if (texto.qtdErros == -1 && texto.qtdPalavras == -1) {
+	    printf ("Nao foi possivel abrir o arquivo de texto %s.\n", arqTexto);
+	    liberaraiz (raiz);
+	    return ARQTEXTO_ERROABERTURA;
+	}
+
+
+	getrusage(RUSAGE_SELF, &tempo_inicial);
+		qtdePalavrasDic = contaDicio (raiz);
+	getrusage(RUSAGE_SELF, &tempo_final);
+
+	tempo_calc_tamanho_dic = calcula_tempo (&tempo_inicial, &tempo_final);
+
+
+	getrusage(RUSAGE_SELF, &tempo_inicial);
+		liberaraiz(raiz);
+	getrusage(RUSAGE_SELF, &tempo_final);
+
+	tempo_limpeza_memoria = calcula_tempo (&tempo_inicial, &tempo_final);
+
+
+	printf("\nTOTAL DE PALAVRAS ERRADAS NO TEXTO      : %d\n",   texto.qtdErros);
+    printf("TOTAL DE PALAVRAS DO DICIONARIO         : %d\n",   qtdePalavrasDic);
+    printf("TOTAL DE PALAVRAS DO TEXTO              : %d\n",   texto.qtdPalavras);
+    printf("TEMPO GASTO COM CARGA DO DICIONARIO     : %.2f\n", tempo_carga);
+    printf("TEMPO GASTO COM CHECK DO ARQUIVO        : %.2f\n", texto.tempoCheck);
+    printf("TEMPO GASTO P CALCULO TAMANHO DICIONARIO: %.2f\n", tempo_calc_tamanho_dic);
+    printf("TEMPO GASTO COM LIMPEZA DA MEMORIA      : %.2f\n", tempo_limpeza_memoria);
+    printf("------------------------------------------------------\n");
+    printf("T E M P O   T O T A L                   : %.2f\n\n",
+     tempo_carga + texto.tempoCheck + tempo_calc_tamanho_dic + tempo_limpeza_memoria);
+    printf("------------------------------------------------------\n");
+
 	return 0;
 }
+
 
 /* #### FUNÇÕES PARA MANIPULAÇÃO DE AVL #### */
 
 
-void carregaDicio (char * filename) {
+bool carregaDicio (char * filename) {
 	FILE *fp = fopen(filename, "r");
 	char c[46];
 
-	if (fp == NULL) return;
+	if (fp == NULL) return false;
 
 	while (fscanf(fp, "%s", c) > 0) {
 		raiz = insert(raiz, c);
@@ -57,30 +140,73 @@ void carregaDicio (char * filename) {
 
 
 	fclose(fp);
+
+	return true;
 }
 
 
-int contaErros (char * filename) {
+tripair contaErros (char * filename) {
 	FILE *fp = fopen(filename, "r");
 	arv * tmp;
-	int ans = 0, i = 0;
-	char c[46], d;
+	int indice = 0;
+	char lowered[46], original[46], c;
+	tripair ans, fail; 
+	struct rusage tempo_inicial, tempo_final;
 
-	if (fp == NULL) return 0;
+	ans.qtdPalavras = ans.qtdErros = ans.tempoCheck = 0;
 
-	for(d = getc(fp); d != EOF; d = getc(fp)) {
-		d = tolower(d);
-		if ((d >= 97 && d <= 122) || d == 39) c[i++] = d;
-		else {
-			c[i++] = '\0';
-			tmp = busca (raiz, c);
-			if (tmp == NULL) ans++;
-			memset(c, 0, sizeof(c)); i = 0;
-		}
+	if (fp == NULL) {
+		fail.qtdPalavras = fail.qtdErros = -1;
+		return fail;
 	}
+
+	for (c = fgetc(fp); c != EOF; c = fgetc(fp)) {
+	    if (isalpha(c) || (c == '\'' && indice > 0)) {
+	    	original[indice] = c;
+	        lowered[indice++] = tolower(c);
+	        if (indice > TAM_MAX) {
+	            while ((c = fgetc(fp)) != EOF && isalpha(c));
+	            indice = 0;
+	        }
+	    } else if (isdigit(c)) {
+	        while ((c = fgetc(fp)) != EOF && isalnum(c));
+	        indice = 0;
+	    } else if (indice > 0) {
+	        original[indice] = lowered[indice] = '\0';
+	        ans.qtdPalavras++;
+
+	        getrusage(RUSAGE_SELF, &tempo_inicial);
+	        	tmp = busca (raiz, lowered);
+	        getrusage(RUSAGE_SELF, &tempo_final);
+	        
+	        ans.tempoCheck += calcula_tempo(&tempo_inicial, &tempo_final);
+
+	        if (tmp == NULL) {
+	            printf("%s\n", original);
+	            ans.qtdErros++;
+	        }
+
+	        memset(original, 0, sizeof(original));
+	        memset(lowered, 0, sizeof(lowered));
+	        indice = 0;
+	    }
+	}
+
 	fclose(fp);
 
 	return ans;
+}
+
+
+double calcula_tempo(const struct rusage *b, const struct rusage *a) {
+    if (b == NULL || a == NULL)
+        return 0;
+    else
+        return ((((a->ru_utime.tv_sec * 1000000 + a->ru_utime.tv_usec) -
+                 (b->ru_utime.tv_sec * 1000000 + b->ru_utime.tv_usec)) +
+                ((a->ru_stime.tv_sec * 1000000 + a->ru_stime.tv_usec) -
+                 (b->ru_stime.tv_sec * 1000000 + b->ru_stime.tv_usec)))
+                / 1000000.0);
 }
 
 
@@ -99,15 +225,6 @@ void liberaraiz (arv * ra) {
 	liberaraiz (ra->dir);
 	free (ra);	
 	return;
-}
-
-
-void set_height (arv * root, int height) {
-	if (root != NULL) {
-		root->h = height;
-		set_height (root->esq, height+1);
-		set_height (root->dir, height+1);
-	}
 }
 
 
@@ -187,4 +304,15 @@ arv * insert (arv * root, char * conteudo) {
 		root = balance (root);
 	}
 	return root;
+}
+
+
+int contaDicio (arv * root) {
+	int res = 0;
+	if (root) {
+		res++;
+		res += contaDicio (root->esq);
+		res += contaDicio (root->dir);
+	}
+	return res;
 }
